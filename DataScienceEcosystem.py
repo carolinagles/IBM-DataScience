849ae12af710e1f988a1f990e2ddba0f416972ef
María Carolina <carolinagles@gmail.com>
2023-06-24 21:29:20 -0700

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# **Objectives:**
+# - Popular Languages in Data Science
+# - Libraries used by Data Scientists
+# - Arithmetic expressions in Python
+
+# Some of the popular languages that Data Scientists use are:
+# 1. Python
+# 2. R
+# 3. Apache Spark
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 
+# 1. ggplot
+# 2. caret
+# 3. stringr
+
+# | Data Science Tools| 
+# | -------------|
+# | RStudio |
+# | TensorFlow | 
+# | Apache Spark |
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+(3*4)+5
+#This a simple arithmetic expression to mutiply then add integers
+
+
+# In[6]:
+
+
+200/60 
+# This will convert 200 minutes to hours by diving by 60
+
+
+# ## Autor   
+# Maria Carolina Gonzalez Bernal
